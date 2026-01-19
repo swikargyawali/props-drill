82dc7a256d5639d16f44b260d16a7f248c733792
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-19 22:03:45 +0545

    changes in the top section

--- props-drill/src/App.jsx
+++ props-drill/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       <div className='card'>
         <div className="top">
           <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" alt="" />
-          <button>Save <Bookmark/></button>
+          <button>Save <Bookmark size={12}/></button>
         </div>
 
         <div className="center">
