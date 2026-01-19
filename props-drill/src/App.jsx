7b06a808d23091ed925e6c40199858ba77172cc2
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-19 23:05:24 +0545

    card

--- props-drill/src/App.jsx
+++ props-drill/src/App.jsx
@@ -1,70 +1,12 @@
-import React from 'react'
-import {Bookmark} from 'lucide-react';
+import Card from "./components/Card";
+import "./index.css";
 
-const App = () => {
+function App() {
   return (
-    <div className='parent'>
-
-      <div className='card'>
-        <div>
-             <div className="top">
-          <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" alt="" />
-          <button>Save <Bookmark size={12}/></button>
-        </div>
-
-        <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
-          
-          <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
-          </div>
-        </div>
-        </div>
-
-        <div className="bottom">
-          
-            <div>
-              <h3>$120/hr</h3>
-              <p>Kathmandu, Nepal</p>
-            </div>
-            <button>Apply Now</button>
-          
-        </div>
-        
-      </div>
-        <div className='card'>
-        <div>
-             <div className="top">
-          <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" alt="" />
-          <button>Save <Bookmark size={12}/></button>
-        </div>
-
-        <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
-          
-          <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
-          </div>
-        </div>
-        </div>
-
-        <div className="bottom">
-          
-            <div>
-              <h3>$120/hr</h3>
-              <p>Kathmandu, Nepal</p>
-            </div>
-            <button>Apply Now</button>
-          
-        </div>
-        
-      </div>
+    <div className="parent">
+      <Card />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
