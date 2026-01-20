fb69f0432a7a721b0a5f96976c25dd4746345e68
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-20 21:39:24 +0545

    User.jsx as an array of object

--- props-drill/src/App.jsx
+++ props-drill/src/App.jsx
@@ -1,12 +1,36 @@
 import Card from "./components/Card";
-import "./index.css";
+import User from "./components/User";
 
-function App() {
-  return (
+import "./index.css";
+const arr=[
+  {user:"Swikar",
+  age:30
+
+  },
+  {user:"Swikar",
+    age:20
+
+  },
+  {user:"Aman",
+    age:24
+  }];
+
+arr.map(function(elem){
+  console.log(elem.age);
+})
+
+
+
+const App=()=>{
+  return(
     <div className="parent">
-      <Card />
+      {/* arr[0], arr[1], arr[2] takes time and space both , so we use map and pass elem ==> map is loop simply*/}
+
+      {/* {arr.map(function(elem){
+        return <h1>{elem/2}</h1>;
+      })} */}
     </div>
-  );
+  )
 }
 
 export default App;
